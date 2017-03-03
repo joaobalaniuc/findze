a0c4a38caaa6d5f2b92e5ac08b7b67b71a11c40a
João Williams Balaniuc <jwillbala@hotmail.com>
2017-03-03 16:31:59 -0300

    master

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -48,13 +48,13 @@ var app = {
         app.receivedEvent('deviceready');
 
         start();
-        
+
         alert(0);
-        
+
         geo();
-        
+
         alert(1);
-        
+
         // SPLASHSCREEN (CONFIG.XML BUGFIX)
         setTimeout(function () {
             navigator.splashscreen.hide();
@@ -76,26 +76,60 @@ var app = {
     }
 };
 
-function geo(cb) {
+function geo() {
+
+    alert("geo0");
+
+    /**
+     * This callback will be executed every time a geolocation is recorded in the background.
+     */
+    var callbackFn = function (location) {
+        alert('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
+
+        // Do your HTTP request here to POST location to your server. 
+        // jQuery.post(url, JSON.stringify(location)); 
+
+        /*
+         IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+         and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+         IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+         */
+        backgroundGeolocation.finish();
+    };
+
+    var failureFn = function (error) {
+        alert('BackgroundGeolocation error');
+    };
+
+    // BackgroundGeolocation is highly configurable. See platform specific configuration options 
+    backgroundGeolocation.configure(callbackFn, failureFn, {
+        desiredAccuracy: 10,
+        stationaryRadius: 20,
+        distanceFilter: 30,
+        interval: 60000
+    });
+
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app. 
+    backgroundGeolocation.start();
+
+    // If you wish to turn OFF background-tracking, call the #stop method. 
+    // backgroundGeolocation.stop(); 
+
+
+    alert("geo1");
 
     var onSuccess = function (position) {
         sessionStorage.lat = position.coords.latitude;
         sessionStorage.lng = position.coords.longitude;
         alert('Latitude: ' + position.coords.latitude + '\n' +
-         'Longitude: ' + position.coords.longitude + '\n' +
-         'Altitude: ' + position.coords.altitude + '\n' +
-         'Accuracy: ' + position.coords.accuracy + '\n' +
-         'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
-         'Heading: ' + position.coords.heading + '\n' +
-         'Speed: ' + position.coords.speed + '\n' +
-         'Timestamp: ' + position.timestamp + '\n');
-
+                'Longitude: ' + position.coords.longitude + '\n' +
+                'Altitude: ' + position.coords.altitude + '\n' +
+                'Accuracy: ' + position.coords.accuracy + '\n' +
+                'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+                'Heading: ' + position.coords.heading + '\n' +
+                'Speed: ' + position.coords.speed + '\n' +
+                'Timestamp: ' + position.timestamp + '\n');
         //alert(sessionStorage.lat + "x" + sessionStorage.lng);
-
-        if (isFunction(cb)) { // isfunction = index.js
-            cb();
-        }
-
     };
     function onError(error) {
         alert('code: ' + error.code + '\n' +
