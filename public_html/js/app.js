6f3bd509d139ec59d9494442c5c4dcc34c0ee1a1
João Williams Balaniuc <jwillbala@hotmail.com>
2017-03-03 16:38:11 -0300

    master

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -80,44 +80,6 @@ function geo() {
 
     alert("geo0");
 
-    /**
-     * This callback will be executed every time a geolocation is recorded in the background.
-     */
-    var callbackFn = function (location) {
-        alert('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
-
-        // Do your HTTP request here to POST location to your server. 
-        // jQuery.post(url, JSON.stringify(location)); 
-
-        /*
-         IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-         and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
-         IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-         */
-        backgroundGeolocation.finish();
-    };
-
-    var failureFn = function (error) {
-        alert('BackgroundGeolocation error');
-    };
-
-    // BackgroundGeolocation is highly configurable. See platform specific configuration options 
-    backgroundGeolocation.configure(callbackFn, failureFn, {
-        desiredAccuracy: 10,
-        stationaryRadius: 20,
-        distanceFilter: 30,
-        interval: 60000
-    });
-
-    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app. 
-    backgroundGeolocation.start();
-
-    // If you wish to turn OFF background-tracking, call the #stop method. 
-    // backgroundGeolocation.stop(); 
-
-
-    alert("geo1");
-
     var onSuccess = function (position) {
         sessionStorage.lat = position.coords.latitude;
         sessionStorage.lng = position.coords.longitude;
@@ -136,4 +98,6 @@ function geo() {
                 'message: ' + error.message + '\n');
     }
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+    alert("geo1");
 }
